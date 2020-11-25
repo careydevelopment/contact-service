aef68c6ca5060b3dc3f97578345694d864316cba
Carey Development, LLC <mrbrianmcarey@gmail.com>
2020-11-24 19:15:17 -0500

    Refactoring

--- src/main/java/com/careydevelopment/ecosystem/user/controller/FileUploaderController.java
+++ src/main/java/com/careydevelopment/ecosystem/user/controller/FileUploaderController.java
@@ -3,7 +3,6 @@ package com.careydevelopment.ecosystem.user.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
