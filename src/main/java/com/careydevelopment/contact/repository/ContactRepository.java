c99c0917d55237cba76d1983efbaa55c304c787c
Carey Development, LLC <mrbrianmcarey@gmail.com>
2020-12-20 14:35:53 -0500

    cleanup work

--- src/main/java/com/careydevelopment/contact/repository/ContactRepository.java
+++ src/main/java/com/careydevelopment/contact/repository/ContactRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.careydevelopment.contact.model.Contact;
-import com.careydevelopment.contact.model.SalesOwner;
 
 @Repository
 public interface ContactRepository extends MongoRepository<Contact, String> {
