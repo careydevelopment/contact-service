687e6b489d0742865bbf91fff51df13412b26fc4
Brian Carey <careyb@mindspring.com>
2021-01-22 04:52:07 -0500

    Logging WebClient requests

--- src/main/java/com/careydevelopment/contact/config/ApplicationListenerInitialize.java
+++ src/main/java/com/careydevelopment/contact/config/ApplicationListenerInitialize.java
@@ -1,14 +1,12 @@
 package com.careydevelopment.contact.config;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
-import com.careydevelopment.contact.service.ContactService;
-import com.careydevelopment.contact.service.ContactService.SaleInfo;
+import com.careydevelopment.contact.model.SalesOwner;
+import com.careydevelopment.contact.service.UserService;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -16,16 +14,16 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class ApplicationListenerInitialize implements ApplicationListener<ApplicationReadyEvent>  {
 	
 	@Autowired
-	private ContactService contactService;
-	 
+	private UserService userService;
+	
     public void onApplicationEvent(ApplicationReadyEvent event) {        	
-    	List<SaleInfo> sales = contactService.getBigSalesPerContact();
+    	SalesOwner owner = userService.fetchUser("Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJkYXJ0aCIsImF1ZCI6ImNhcmV5ZGV2ZWxvcG1lbnQtZWNvc3lzdGVtLXVzZXJzIiwiaWQiOiI1Zjc4ZDhmYmMxZDMyNDZhYjQzMDNmMmIiLCJleHAiOjE2MTEzNDc5NDMsImlhdCI6MTYxMTI2MTU0MywiYXV0aG9yaXRpZXMiOlsiQ0FSRVlERVZFTE9QTUVOVF9DUk1fVVNFUiJdfQ.1x9rkEB3abEjMiRRtvSwgg4FFLPLdVT-jNDIztIRFF2dgqn50IVR50wmNXcinLBqSzD0KS_q1Uf6kbXA6UBgOQ");
     	
     	try {
 	    	ObjectMapper objectMapper = new ObjectMapper();
 	    	objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
 	    	objectMapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
-	    	System.err.println(objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(sales));
+	    	System.err.println(objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(owner));
     	} catch (Exception e) {
     		e.printStackTrace();
     	}
