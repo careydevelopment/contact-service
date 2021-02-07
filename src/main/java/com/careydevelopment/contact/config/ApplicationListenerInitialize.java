ca57cce74be43438f37a9a9495e2364bb1be2906
Carey Development, LLC <mrbrianmcarey@gmail.com>
2021-02-07 14:58:26 -0500

    Update ApplicationListenerInitialize.java

--- src/main/java/com/careydevelopment/contact/config/ApplicationListenerInitialize.java
+++ src/main/java/com/careydevelopment/contact/config/ApplicationListenerInitialize.java
@@ -18,7 +18,7 @@ public class ApplicationListenerInitialize implements ApplicationListener<Applic
     
     public void onApplicationEvent(ApplicationReadyEvent event) {
         try {
-            SalesOwner owner = userService.fetchUser("Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJkYXJ0aCIsImF1ZCI6ImNhcmV5ZGV2ZWxvcG1lbnQtZWNvc3lzdGVtLXVzZXJzIiwiaWQiOiI1Zjc4ZDhmYmMxZDMyNDZhYjQzMDNmMmIiLCJleHAiOjE2MTE3OTAyMDAsImlhdCI6MTYxMTcwMzgwMCwiYXV0aG9yaXRpZXMiOlsiQ0FSRVlERVZFTE9QTUVOVF9DUk1fVVNFUiJdfQ.ENmihdJQkscMN0IAnjvnnM2Ys5QweTQ_-CmaOL9DXut9eiVtrAgo2pUDXQFcPMemA7YY2aQt8h7TlkWzVyIeMg");
+            SalesOwner owner = userService.fetchUser("Bearer eyJhbGciOiJIUz...");
             ObjectMapper objectMapper = new ObjectMapper();
             System.err.println(objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(owner));
         } catch (ServiceException e) {
